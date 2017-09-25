c626024df7b3a058b156317832aa0063e5b05c96
Scott <syhlion@gmail.com>
2017-09-25 18:24:47 +0800

    fix

--- number.go
+++ number.go
@@ -1,33 +1,37 @@
 package simplenum
 
 import (
-	"fmt"
-	"math"
-	"strconv"
+	"github.com/shopspring/decimal"
 )
 
 //四捨五入
 func Round(v float64, decimals int) float64 {
-	var pow float64 = 1
-
-	for i := 0; i < decimals; i++ {
-		pow *= 10
-	}
-	return float64(int((v*pow)+0.5)) / pow
+	dv := decimal.NewFromFloat(v)
+	dv = dv.Round(int32(decimals))
+	a, _ := dv.Float64()
+	return a
 }
 
 //無條件進位
 func Ceil(v float64, decimals int) float64 {
-	var pow float64 = 1
-	for i := 0; i < decimals; i++ {
-		pow *= 10
-	}
-	return float64(math.Ceil(v*pow)) / pow
+	pow := decimal.NewFromFloat(10)
+	pow = pow.Pow(decimal.NewFromFloat(float64(decimals)))
+	dv := decimal.NewFromFloat(v)
+	ans := dv.Mul(pow)
+	ans = ans.Ceil()
+	ans = ans.Div(pow)
+	a, _ := ans.Float64()
+	return a
 }
 
 //無條件捨去
 func Floor(v float64, decimals int) float64 {
-	fs := fmt.Sprintf("%0."+strconv.Itoa(decimals)+"f", v)
-	s, _ := strconv.ParseFloat(fs, 64)
-	return s
+	pow := decimal.NewFromFloat(10)
+	pow = pow.Pow(decimal.NewFromFloat(float64(decimals)))
+	dv := decimal.NewFromFloat(v)
+	ans := dv.Mul(pow)
+	ans = ans.Floor()
+	ans = ans.Div(pow)
+	a, _ := ans.Float64()
+	return a
 }
